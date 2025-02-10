6dddd0831ed4c14421ae98592aac54ab0d906b14
Adson Souza <ty.msn@hotmail.com>
2025-02-10 18:22:10 -0300

    Initial structure

---
+++ src/components/Dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard">
+    <h1>Pedidos de Viagem</h1>
+   
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Dashboard',
+  components: {
+
+  },
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style scoped>
+.dashboard {
+  padding: 1rem;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 1rem;
+}
+th, td {
+  border: 1px solid #ccc;
+  padding: 0.5rem;
+}
+</style>
